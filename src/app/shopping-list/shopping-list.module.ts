a8e87d5dbe53c3e602391c0764a2d125582fd780
Rockott <777888000d@gmail.com>
2019-11-27 16:03:24 +0200

    coreModule

--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
 
 import { ShoppingListComponent } from './shopping-list.component';
 import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
@@ -12,11 +12,11 @@ import { FormsModule } from '@angular/forms';
         ShoppingEditComponent
     ],
     imports: [
-        CommonModule,
         FormsModule,
         RouterModule.forChild([
             { path: "shopping-list", component: ShoppingListComponent }
-        ])
+        ]),
+        SharedModule
     ]
 })
 export class ShoppingListModule { }
