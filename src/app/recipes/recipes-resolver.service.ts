20d6bac848eae074cef8356fc4dcc174982a4a1d
Rockott <777888000d@gmail.com>
2019-12-12 11:26:09 +0200

    Update, Delete and Add Recipes

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -2,29 +2,36 @@ import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Actions, ofType } from '@ngrx/effects';
-import { take } from 'rxjs/operators';
+import { take, map, switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 import { Recipe } from './recipe.model';
 import { RecipeService } from './recipe.service';
 import * as fromApp from '../store/app.reducer';
-import * as RecipeActions from '../recipes/store/recipe.actions';
+import * as RecipesActions from '../recipes/store/recipe.actions';
 
 @Injectable({ providedIn: "root" })
 export class RecipesResolverService implements Resolve<Recipe[]> {
     constructor(private store: Store<fromApp.AppState>, private actions$: Actions, private recipeService: RecipeService) { }
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-
-        this.store.dispatch(new RecipeActions.FetchRecipes());
-        return this.actions$.pipe(ofType(RecipeActions.SET_RECIPES), take(1));
-
-        // const recipes = this.recipeService.getRecipes();
-
-        // if (recipes.length === 0) {
-        //     return this.dataStorageService.fetchRecipes();
-        // } else {
-        //     return recipes;
-        // }
-
+        // return this.dataStorageService.fetchRecipes();
+        return this.store.select('recipes').pipe(
+            take(1),
+            map(recipesState => {
+                return recipesState.recipes;
+            }),
+            switchMap(recipes => {
+                if (recipes.length === 0) {
+                    this.store.dispatch(new RecipesActions.FetchRecipes());
+                    return this.actions$.pipe(
+                        ofType(RecipesActions.SET_RECIPES),
+                        take(1)
+                    );
+                } else {
+                    return of(recipes);
+                }
+            })
+        );
     }
 }
